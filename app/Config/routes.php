730caa85c9857b8be8c5b5b005b1215502dd1b39
mrsaooty <mrsaooty@gmail.com>
2015-01-16 18:57:19 +0100

    Ajout de la gestion du serveur

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -62,6 +62,7 @@
 	Router::connect('/admin/donator_chart', ['controller' => 'pages', 'action' => 'donator_chart', 'admin' => true]);
 	Router::connect('/admin/memory_chart', ['controller' => 'pages', 'action' => 'memory_chart', 'admin' => true]);
 	Router::connect('/admin/list_donator', ['controller' => 'pages', 'action' => 'list_donator', 'admin' => true]);
+	Router::connect('/admin/players', ['controller' => 'players', 'action' => 'index', 'admin' => true]);
 	Router::connect('/:slug-:id', ['controller' => 'posts', 'action' => 'read'], ['pass' => ['slug', 'id'], 'slug' => '[a-z0-9\-]+', 'id' => '[0-9]+']);
 
 	/* Paypal IPN plugin */
